901bbc60b180bf5f42b044f40c9e979ff8cf7043
pro777max <pro777max@github.com>
2026-09-22 22:13:23 +0300

    init: lvgl gauges skeleton

---
+++ main/main.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "bsp/esp-bsp.h"
+#include "lvgl.h"
+
+static lv_obj_t *spd_arc, *rpm_arc, *spd_lbl, *rpm_lbl;
+static int spd = 0, rpm = 800;
+
+static void gauge_setup(lv_obj_t *arc, lv_align_t align, int32_t dx, int max) {
+    lv_arc_set_range(arc, 0, max);
+    lv_arc_set_value(arc, 0);
+    lv_obj_set_size(arc, 320, 320);
+    lv_obj_align(arc, align, dx, 0);
+    lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
+    lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE);
+}
+
+static void ui_timer_cb(lv_timer_t *t) {
+    spd += (rand() % 9) - 4;
+    if (spd < 0) spd = 0;
+    if (spd > 220) spd = 220;
+    rpm += (rand() % 501) - 250;
+    if (rpm < 800) rpm = 800;
+    if (rpm > 7500) rpm = 7500;
+    bsp_display_lock(0);
+    lv_arc_set_value(spd_arc, spd);
+    lv_arc_set_value(rpm_arc, rpm / 100);
+    lv_label_set_text_fmt(spd_lbl, "%d", spd);
+    lv_label_set_text_fmt(rpm_lbl, "%d", rpm);
+    bsp_display_unlock();
+}
+
+void app_main(void) {
+    bsp_display_start();
+    bsp_display_lock(0);
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_black(), 0);
+    spd_arc = lv_arc_create(lv_screen_active());
+    gauge_setup(spd_arc, LV_ALIGN_LEFT_MID, 60, 220);
+    rpm_arc = lv_arc_create(lv_screen_active());
+    gauge_setup(rpm_arc, LV_ALIGN_RIGHT_MID, -60, 75);
+    spd_lbl = lv_label_create(lv_screen_active());
+    lv_obj_align_to(spd_lbl, spd_arc, LV_ALIGN_CENTER, 0, 0);
+    rpm_lbl = lv_label_create(lv_screen_active());
+    lv_obj_align_to(rpm_lbl, rpm_arc, LV_ALIGN_CENTER, 0, 0);
+    lv_timer_create(ui_timer_cb, 100, NULL);
+    bsp_display_unlock();
+}
