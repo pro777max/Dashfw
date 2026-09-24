0a351c3adaf82b85b5d325528caf1b39bf46b069
pro777max <pro777max@github.com>
2026-09-24 14:39:06 +0300

    test: force backlight on + red screen check

--- main/main.c
+++ main/main.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
+#include "esp_log.h"
 #include "bsp/esp-bsp.h"
 #include "lvgl.h"
 
+static const char *TAG = "DASH";
 static lv_obj_t *spd_arc, *rpm_arc, *spd_lbl, *rpm_lbl;
 static int spd = 0, rpm = 800;
 
@@ -31,8 +33,11 @@ static void ui_timer_cb(lv_timer_t *t) {
 
 void app_main(void) {
     bsp_display_start();
+    esp_err_t bl = bsp_display_backlight_on();
+    ESP_LOGI(TAG, "backlight_on: %s", esp_err_to_name(bl));
     bsp_display_lock(0);
-    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_black(), 0);
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0xff0000), 0);
+    lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_COVER, 0);
     spd_arc = lv_arc_create(lv_screen_active());
     gauge_setup(spd_arc, LV_ALIGN_LEFT_MID, 60, 220);
     rpm_arc = lv_arc_create(lv_screen_active());
@@ -43,4 +48,5 @@ void app_main(void) {
     lv_obj_align_to(rpm_lbl, rpm_arc, LV_ALIGN_CENTER, 0, 0);
     lv_timer_create(ui_timer_cb, 100, NULL);
     bsp_display_unlock();
+    ESP_LOGI(TAG, "ui ready");
 }
